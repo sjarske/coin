702c5af9bf5251253bed9a92b4e1cd99fa02d876
sjors van de pol <s.vandepol@student.fontys.nl>
2021-10-05 13:24:59 +0200

    Update CoinApplicationTests.java

--- src/test/java/com/coin/coin/CoinApplicationTests.java
+++ src/test/java/com/coin/coin/CoinApplicationTests.java
@@ -3,11 +3,14 @@ package com.coin.coin;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @SpringBootTest
 class CoinApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(1).isEqualTo(1);
 	}
 
 }
