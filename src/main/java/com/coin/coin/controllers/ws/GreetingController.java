1b2fbea18e44821ae627bfb7f2190b50f6189999
sjors van de pol <s.vandepol@student.fontys.nl>
2022-01-03 16:14:14 +0100

    trading pair websocket

--- src/main/java/com/coin/coin/controllers/ws/GreetingController.java
+++ src/main/java/com/coin/coin/controllers/ws/GreetingController.java
@@ -1,5 +1,8 @@
 package com.coin.coin.controllers.ws;
 
+import com.binance.api.client.BinanceApiClientFactory;
+import com.binance.api.client.BinanceApiRestClient;
+import com.binance.api.client.domain.market.TickerStatistics;
 import com.coin.coin.models.ws.Greeting;
 import com.coin.coin.models.ws.Message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -13,6 +16,15 @@ public class GreetingController {
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Greeting greeting(Message message) {
+        if (message.getName().contains("pair:")){
+            String content = message.getName().replaceFirst("pair:", "");
+            System.out.println(content);
+            BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance();
+            BinanceApiRestClient client = factory.newRestClient();
+
+            TickerStatistics tickerStatistics = client.get24HrPriceStatistics(content);
+            message.setName(tickerStatistics.getLastPrice());
+        }
         return new Greeting(HtmlUtils.htmlEscape(message.getName()));
     }
 
